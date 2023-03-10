a5ee28697149174ed26004ebc8b35c9366791a03
abhijit57 <nabhijit787@gmail.com>
2023-03-09 20:32:02 -0500

    EDA and Modelling in Notebooks

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
 # This program creates custom exception handling
 
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()    # Throws which file and which line an exception is occurring in
@@ -21,11 +21,10 @@ class CustomException(Exception):
         return self.error_message
     
 
+# if __name__ == "__main__":
 
-if __name__ == "__main__":
-
-    try:
-        a=1/0
-    except Exception as e:
-        logging.error("Divide by Zero Exception Error")
-        raise CustomException(e, sys)
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divide by Zero Exception Error")
+#         raise CustomException(e, sys)
