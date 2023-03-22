76b98fafc752c4ce84b4c01d7fd861874fdf9b72
abhijit57 <nabhijit787@gmail.com>
2023-03-22 11:12:33 -0400

    prediction pipeline changes

--- src/pipeline/predict_piepline.py
+++ src/pipeline/predict_piepline.py
@@ -17,10 +17,12 @@ class PredictPipeline:
             preprocessor=load_object(file_path=preprocessor_path)
             data_scaled=preprocessor.transform(features)
             preds=model.predict(data_scaled)
+
+            return preds
+        
         except Exception as e:
             raise CustomException(e, sys)
 
-        return preds
 
 
 class CustomData:
